1077b3c1220435c989e40f942156ec0f008a5cc0
Nuria Campo <nuria.campo7@gmail.com>
2024-11-28 13:45:32 +0100

    styles 'volver'

--- src/components/CharacterDetails.jsx
+++ src/components/CharacterDetails.jsx
@@ -10,26 +10,32 @@ function CharacterDetails({ characters }) {
   });
 
   return (
-    <section className="containerDetails">
-      <div className="details">
-        <img src={selectedCharacter.photo} alt={selectedCharacter.name} />
-        <div className="details__text">
-          <h3>{selectedCharacter.name}</h3>
-          <p>
-            <b>Status</b>: {selectedCharacter.status}
-          </p>
-          <p>
-            <b>Species</b>: {selectedCharacter.species}
-          </p>
-          <p>
-            <b>Origin</b>: {selectedCharacter.planet}
-          </p>
-          <p>
-            <b>Episodies</b>: {selectedCharacter.episodies}
-          </p>
+    <>
+      <p className="details__p">
+        {" "}
+        <i class="fa-solid fa-less-than details__p__icon"></i> Volver
+      </p>
+      <section className="containerDetails">
+        <div className="details">
+          <img src={selectedCharacter.photo} alt={selectedCharacter.name} />
+          <div className="details__text">
+            <h3>{selectedCharacter.name}</h3>
+            <p>
+              <b>Status</b>: {selectedCharacter.status}
+            </p>
+            <p>
+              <b>Species</b>: {selectedCharacter.species}
+            </p>
+            <p>
+              <b>Origin</b>: {selectedCharacter.planet}
+            </p>
+            <p>
+              <b>Episodies</b>: {selectedCharacter.episodies}
+            </p>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 }
 

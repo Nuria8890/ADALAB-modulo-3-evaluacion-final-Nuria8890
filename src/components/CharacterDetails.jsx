27ea91310187c75111fbc667cf988d8e3129de4b
Nuria Campo <nuria.campo7@gmail.com>
2024-11-28 14:35:40 +0100

    intent icons...

--- src/components/CharacterDetails.jsx
+++ src/components/CharacterDetails.jsx
@@ -2,6 +2,10 @@ import PropTypes from "prop-types";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import "../styles/layout/CharacterDetails.scss";
+// import { library } from "@fortawesome/fontawesome-svg-core";
+// import { all } from "@awesome.me/kit-KIT_CODE/icons";
+// library.add(...all);
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function CharacterDetails({ characters }) {
   const { idCharacter } = useParams();
@@ -27,6 +31,7 @@ function CharacterDetails({ characters }) {
             </p>
             <p>
               <b>Species</b>: {selectedCharacter.species}
+              {/* <FontAwesomeIcon icon="fa-brands fa-reddit-alien" /> */}
             </p>
             <p>
               <b>Origin</b>: {selectedCharacter.planet}

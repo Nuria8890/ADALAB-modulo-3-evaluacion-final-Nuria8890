8a33bac59039cfd04a1eadc69df3e93899a1c09a
Nuria Campo <nuria.campo7@gmail.com>
2024-11-29 13:17:40 +0100

    icons in cards

--- src/components/List/CharacterCard.jsx
+++ src/components/List/CharacterCard.jsx
@@ -2,14 +2,32 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import "../../styles/layout/CharacterList.scss";
 
+import { RxAvatar } from "react-icons/rx";
+import { PiAlienFill } from "react-icons/pi";
+
 function CharacterCard({ character }) {
+  let specie;
+  if (character.species === "Human") {
+    specie = (
+      <span>
+        Humano <RxAvatar />
+      </span>
+    );
+  } else {
+    specie = (
+      <span>
+        Alien <PiAlienFill />
+      </span>
+    );
+  }
+
   return (
     <li className="list__item">
       <Link to={`/characters/${character.id}`}>
         <img src={character.photo} alt={character.name} />
         <div className="list__item__text">
           <h4>{character.name}</h4>
-          <p>{character.species === "Human" ? "Humano" : character.species}</p>
+          <p>{specie}</p>
         </div>
       </Link>
     </li>
